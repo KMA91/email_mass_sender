88eeea8c8cf19969c41dd9b9a2cda80bf0e42690
Kevin <kevinma91@yahoo.com>
2017-10-25 11:02:10 -0800

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({hi:"there"})
+  res.send({ bye: 'buddy'})
 });
 
 // Use Heroku env/port(prod) OR 5000(dev)
