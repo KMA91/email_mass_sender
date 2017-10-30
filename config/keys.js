fce97ba447a0e0a6802c3a69efa6ecc4bdc20bfb
Kevin <kevinma91@yahoo.com>
2017-10-30 13:34:17 -0800

    update

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,6 @@
 // Checks for environment settings to use keys
-module.exports = {
-  if(process.env.NODE_ENV === 'production'){
-    module.exports = require('./prod');
-  }else{
-    module.exports = require('./dev');
-  }
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./prod');
+}else{
+  module.exports = require('./dev');
 }
