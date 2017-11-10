c5221283727641037b5adbe4ded95ddf2daf5d37
Kevin <kevinma91@yahoo.com>
2017-11-09 18:57:33 -0800

    fixed id bug

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -11,7 +11,7 @@ class SurveyList extends Component {
   renderSurveys() {
     return this.props.surveys.reverse().map(survey => {
       return (
-        <div className="card blue-grey white-text" key="survey._id">
+        <div className="card blue-grey white-text" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{ survey.title }</span>
             <p>{ survey.body }</p>
